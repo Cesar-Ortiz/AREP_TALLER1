3c47a8a66f924c10df67f0fd909ec16731d771b2
César Ortiz <cesar.ortiz@mail.escuelaing.edu.co>
2021-08-23 23:52:18 -0500

    corrigiendo apiclient

--- src/main/resources/public/appiclient.js
+++ src/main/resources/public/appiclient.js
@@ -1,13 +1,10 @@
 const appiclient = (function () {
-    let localhost = "http://localhost:4567";
 
     function getIntraday(symbol, tm, callback) {
 
         const promise = new Promise((resolve, reject) => {
             $.ajax({
-                url: localhost + "/getTime/" + symbol + "/"+tm,
-                type: 'GET',
-                contentType: "application/json"
+                url: "/getTime/" + symbol + "/"+tm
             }).done(function (response) {
                 resolve(response);
             }).fail(function (msg) {
